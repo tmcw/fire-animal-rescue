3aa53e804c5a470eae1f462232d92f43a81d1876
Tom MacWright <tom@macwright.org>
2020-09-11 22:57:02 -0700

    Add LRU cache. Fixes #4

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const d3 = require("d3");
+const LRU = require("lru-cache");
 const fetch = require("node-fetch");
 const usZips = require("us-zips");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const options = {
+  maxAge: 1000 * 60 * 5,
+};
+const cache = new LRU(options);
+
 async function getSheet(token) {
   const url = `https://docs.google.com/spreadsheets/d/${token}/gviz/tq?tqx=out:csv&sheet=Sheet1`;
-  console.log(url);
   return fetch(url)
     .then((r) => r.text())
     .then((text) => {
@@ -46,19 +51,43 @@ async function getSheet(token) {
 }
 
 app.get("/can_help.geojson", (_req, res) => {
-  getSheet(process.env.CAN_HELP_TOKEN).then((json) => res.send(json));
+  const cached = cache.get("can_help");
+  if (cached) {
+    res.send(cached);
+  } else {
+    getSheet(process.env.CAN_HELP_TOKEN).then((json) => {
+      cache.set("can_help", json);
+      res.send(json);
+    });
+  }
 });
 
 app.get("/have_room.geojson", (_req, res) => {
-  getSheet(process.env.HAVE_ROOM_TOKEN).then((json) => res.send(json));
+  const cached = cache.get("have_room");
+  if (cached) {
+    res.send(cached);
+  } else {
+    getSheet(process.env.HAVE_ROOM_TOKEN).then((json) => {
+      cache.set("have_room", json);
+      res.send(json);
+    });
+  }
 });
 
 app.get("/need_help.geojson", (_req, res) => {
-  getSheet(process.env.NEED_HELP_TOKEN).then((json) => res.send(json));
+  const cached = cache.get("need_help");
+  if (cached) {
+    res.send(cached);
+  } else {
+    getSheet(process.env.NEED_HELP_TOKEN).then((json) => {
+      cache.set("need_help", json);
+      res.send(json);
+    });
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  // console.log(`Example app listening at http://localhost:${port}`);
 });
 
 app.use(express.static("public"));
